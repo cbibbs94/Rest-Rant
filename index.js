5a86882f882cc98a091fe7d3c189c952e7287819
cbibbs94 <89940267+cbibbs94@users.noreply.github.com>
2022-01-12 13:54:10 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// index file
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Welcome to this outstanding page. What do you want??")
+})
+
+app.listen(3000, () => {
+     console.log("Shhhhh They're here, Look professional")
+})
