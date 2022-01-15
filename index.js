2ee63ca9e2f7459a0212fe52f0187b41a1f62f4e
cbibbs94 <89940267+cbibbs94@users.noreply.github.com>
2022-01-15 11:33:45 -0800

    adding css files and linking them

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+//Modules and Global Variables
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//Middleware settings
+app.set('views',__dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+//Controllers and routes
 app.use('/places', require('./controllers/places') )
 
 app.get('/', (req, res) => {
@@ -15,6 +20,8 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
+
+//Listen for server connection
 app.listen(process.env.PORT, () => {
      console.log("Shhhhh They're here, Look professional")
 })
