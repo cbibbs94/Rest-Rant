ccd1c2dd390f20af880d964bf7ce9e2463f25ef1
cbibbs94 <89940267+cbibbs94@users.noreply.github.com>
2022-01-18 12:46:32 -0800

    added css and a janky 404 page

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,9 +6,12 @@ function error404() {
         <Def>
             <main>
                 <h1>404: Bruh, What are you even doing??</h1>
-                <p>You know damn well you're not supposed to be here. Turn around and leave the way you came and there won't be any trouble alright?</p>
 
             </main>
+            <body>
+            <h3>You know damn well you're not supposed to be here. Turn around and leave the way you came and there won't be any trouble alright?</h3>
+            <iframe src="https://giphy.com/embed/Pl4PHOXVpPPDG" width="480" height="252" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/dont-be-a-menace-movie-Pl4PHOXVpPPDG">via GIPHY</a></p>
+            </body>
         </Def>
     )
 }
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,13 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="\images\heather-barnes-8yji1QSu58U-unsplash.jpg" alt="Empty Restaurant" width ="300" height="300" />
+                    <div>
+                    Photo by <a href="https://unsplash.com/@nickkarvounis?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nick Karvounis</a> on <a href="https://unsplash.com/s/photos/restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+                    </div>
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
